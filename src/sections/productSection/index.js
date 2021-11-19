d3e05bf4c968e41e7dc596a0a7548f629ab17bda
coders003 <giri.shashank19@gmail.com>
2021-11-19 14:16:09 +0530

    update issue

--- src/sections/productSection/index.js
+++ src/sections/productSection/index.js
@@ -1,51 +1,52 @@
+// eslint-disable-next-line no-unused-vars
 import "./style.css";
-import Tab1 from "../../assets/images/tab1-logo.png";
-import Tab2 from "../../assets/images/tab2-logo.png";
-import Tab3 from "../../assets/images/tab3-logo.png";
-import TabImage from "../../assets/images/tabimage.png";
+// import Tab1 from "../../assets/images/tab1-logo.png";
+// import Tab2 from "../../assets/images/tab2-logo.png";
+// import Tab3 from "../../assets/images/tab3-logo.png";
+// import TabImage from "../../assets/images/tabimage.png";
 import { Title } from "../../components";
 
-const data = [
-  {
-    tab: {
-      logo: Tab1,
-      title: "Unitrade",
-      desc: "DeFi Trading Platform Built from Uniswap",
-    },
-    content: {
-      image: TabImage,
-      title: "Unitrade",
-      site: "unitrade.app",
-      desc: "Schedule Buys / Sells, No KYC or Limits, Professional Trading Tools, Completely Decentralized",
-    },
-  },
-  {
-    tab: {
-      logo: Tab2,
-      title: "Unitrade",
-      desc: "DeFi Trading Platform Built from Uniswap",
-    },
-    content: {
-      image: TabImage,
-      title: "Unitrade",
-      site: "unitrade.app",
-      desc: "Schedule Buys / Sells, No KYC or Limits, Professional Trading Tools, Completely Decentralized",
-    },
-  },
-  {
-    tab: {
-      logo: Tab3,
-      title: "Unitrade",
-      desc: "DeFi Trading Platform Built from Uniswap",
-    },
-    content: {
-      image: TabImage,
-      title: "Unitrade",
-      site: "unitrade.app",
-      desc: "Schedule Buys / Sells, No KYC or Limits, Professional Trading Tools, Completely Decentralized",
-    },
-  },
-];
+// const data = [
+//   {
+//     tab: {
+//       logo: Tab1,
+//       title: "Unitrade",
+//       desc: "DeFi Trading Platform Built from Uniswap",
+//     },
+//     content: {
+//       image: TabImage,
+//       title: "Unitrade",
+//       site: "unitrade.app",
+//       desc: "Schedule Buys / Sells, No KYC or Limits, Professional Trading Tools, Completely Decentralized",
+//     },
+//   },
+//   {
+//     tab: {
+//       logo: Tab2,
+//       title: "Unitrade",
+//       desc: "DeFi Trading Platform Built from Uniswap",
+//     },
+//     content: {
+//       image: TabImage,
+//       title: "Unitrade",
+//       site: "unitrade.app",
+//       desc: "Schedule Buys / Sells, No KYC or Limits, Professional Trading Tools, Completely Decentralized",
+//     },
+//   },
+//   {
+//     tab: {
+//       logo: Tab3,
+//       title: "Unitrade",
+//       desc: "DeFi Trading Platform Built from Uniswap",
+//     },
+//     content: {
+//       image: TabImage,
+//       title: "Unitrade",
+//       site: "unitrade.app",
+//       desc: "Schedule Buys / Sells, No KYC or Limits, Professional Trading Tools, Completely Decentralized",
+//     },
+//   },
+// ];
 
 const ProductSection = () => {
   return (
